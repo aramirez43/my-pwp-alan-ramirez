31ac7c74728e4c9a13d944d943e72425c55f4f6b
Alan Ramirez <aramireznicola@cnm.edu>
2021-11-05 10:35:46 -0600

    added mailgun and recaptcha keys also fixed index js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const Recaptcha = require("express-recaptcha").RecaptchaV2
 const formData = require("form-data")
 const Mailgun = require("mailgun.js")
 const mailgun = new Mailgun(formData)
+require('dotenv').config()
 const {check, validationResult} = require("express-validator")
 const {request, response} = require("express");
 
@@ -16,7 +17,7 @@ const validation = [
 ]
 
 const app = express()
-const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY_KEY, process.env.RECAPTCHA_SECRET_KEY)
+const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
 const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY})
 
 app.use(morgan("dev"))
